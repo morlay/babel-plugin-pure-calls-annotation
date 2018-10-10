bab7b861c8b7ad14f32de25617470519f8513d95
Morlay <morlay.null@gmail.com>
2018-10-10 23:31:52 +0800

    rename

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,68 +1,78 @@
-import { transform } from "babel-core"
-import pluginAnnotatePureCallInVariableDeclarator from "../"
+import { transform } from "babel-core";
+import pluginAnnotatePureCallInVariableDeclarator from "../";
 
-const cases = [{
-  title: "Annotated #__PURE__",
-  src: `const A = String("a");`,
-  dest: `const A = /*#__PURE__*/String("a");`,
-}, {
-  title: "Annotated #__PURE__ in assignment expression",
-  src: `A = String("a");`,
-  dest: `A = /*#__PURE__*/String("a");`,
-}, {
-  title: "Annotated #__PURE__ multiline",
-  src: `const A = String("a");
+const cases = [
+  {
+    title: "Annotated #__PURE__",
+    src: `const A = String("a");`,
+    dest: `const A = /*#__PURE__*/String("a");`,
+  },
+  {
+    title: "Annotated #__PURE__ in assignment expression",
+    src: `A = String("a");`,
+    dest: `A = /*#__PURE__*/String("a");`,
+  },
+  {
+    title: "Annotated #__PURE__ multiline",
+    src: `const A = String("a");
 const B = String("b");`,
-  dest: `const A = /*#__PURE__*/String("a");
+    dest: `const A = /*#__PURE__*/String("a");
 const B = /*#__PURE__*/String("b");`,
-}, {
-  title: "Skip #__PURE__ for side effect fn()",
-  src: `fn();`,
-  dest: `fn();`,
-}, {
-  // only: true,
-  title: "Skip #__PURE__ for side effect fn()()()",
-  src: `fn()()();`,
-  dest: `fn()()();`,
-}, {
-  title: "Skip annotated #__PURE__ when already have",
-  src: `const A = /*#__PURE__*/String("a");`,
-  dest: `const A = /*#__PURE__*/String("a");`,
-}, {
-  title: "Annotated #__PURE__ with other comments",
-  src: `const A = /*other comments*/String("a");`,
-  dest: `const A = /*other comments*/ /*#__PURE__*/String("a");`,
-}, {
-  title: "Annotated #__PURE__ when export",
-  src: `export const A = String("a");`,
-  dest: `export const A = /*#__PURE__*/String("a");`,
-}, {
-  title: "Annotated #__PURE__ for IIFE",
-  src: `export const A = (() => "a")();`,
-  dest: `export const A = /*#__PURE__*/(() => "a")();`,
-}, {
-  title: "Annotated #__PURE__ for import()",
-  src: `export const A = import("")`,
-  dest: `export const A = /*#__PURE__*/import("");`,
-}, {
-  title: "Annotated #__PURE__ for fn(fn())",
-  src: `export const A = fn(fn())`,
-  dest: `export const A = /*#__PURE__*/fn( /*#__PURE__*/fn());`,
-}]
+  },
+  {
+    title: "Skip #__PURE__ for side effect fn()",
+    src: `fn();`,
+    dest: `fn();`,
+  },
+  {
+    // only: true,
+    title: "Skip #__PURE__ for side effect fn()()()",
+    src: `fn()()();`,
+    dest: `fn()()();`,
+  },
+  {
+    title: "Skip annotated #__PURE__ when already have",
+    src: `const A = /*#__PURE__*/String("a");`,
+    dest: `const A = /*#__PURE__*/String("a");`,
+  },
+  {
+    title: "Annotated #__PURE__ with other comments",
+    src: `const A = /*other comments*/String("a");`,
+    dest: `const A = /*other comments*/ /*#__PURE__*/String("a");`,
+  },
+  {
+    title: "Annotated #__PURE__ when export",
+    src: `export const A = String("a");`,
+    dest: `export const A = /*#__PURE__*/String("a");`,
+  },
+  {
+    title: "Annotated #__PURE__ for IIFE",
+    src: `export const A = (() => "a")();`,
+    dest: `export const A = /*#__PURE__*/(() => "a")();`,
+  },
+  {
+    title: "Annotated #__PURE__ for import()",
+    src: `export const A = import("")`,
+    dest: `export const A = /*#__PURE__*/import("");`,
+  },
+  {
+    title: "Annotated #__PURE__ for fn(fn())",
+    src: `export const A = fn(fn())`,
+    dest: `export const A = /*#__PURE__*/fn( /*#__PURE__*/fn());`,
+  },
+];
 
 function unPad(str: string) {
-  return str.replace(/^\n+|\n+$/, "").replace(/\n+/g, "\n")
+  return str.replace(/^\n+|\n+$/, "").replace(/\n+/g, "\n");
 }
 
 describe("test cases", () => {
   cases.forEach((caseItem) => {
     ((caseItem as any).only ? it.only : it)(caseItem.title, () => {
       const code = transform(caseItem.src, {
-        plugins: [
-          pluginAnnotatePureCallInVariableDeclarator,
-        ],
-      }).code
-      expect(unPad(code || "")).toEqual(unPad(caseItem.dest))
-    })
-  })
-})
+        plugins: [pluginAnnotatePureCallInVariableDeclarator],
+      }).code;
+      expect(unPad(code || "")).toEqual(unPad(caseItem.dest));
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,51 @@
-import * as syntax from "babel-plugin-syntax-dynamic-import"
-import {
-  Node,
-  NodePath,
-} from "babel-traverse"
-import {
-  Comment,
-  CommentBlock,
-} from "babel-types"
+import * as syntax from "babel-plugin-syntax-dynamic-import";
+import { Node, NodePath } from "babel-traverse";
+import { Comment, CommentBlock } from "babel-types";
 
-const PURE_ANNOTATION = "#__PURE__"
+const PURE_ANNOTATION = "#__PURE__";
 
 const isPureAnnotated = (comments?: Comment[]): boolean => {
   if (typeof comments === "undefined") {
-    return false
+    return false;
   }
   if (comments.length > 0) {
-    return comments[comments.length - 1].value === PURE_ANNOTATION
+    return comments[comments.length - 1].value === PURE_ANNOTATION;
   }
-  return false
-}
+  return false;
+};
 
-const createComponentBlock = (value: string): Comment => ({
-  type: "CommentBlock",
-  value,
-}) as CommentBlock
+const createComponentBlock = (value: string): Comment =>
+  ({
+    type: "CommentBlock",
+    value,
+  } as CommentBlock);
 
 export default () => ({
   inherits: syntax,
   visitor: {
     CallExpression: {
       enter(nodePath: NodePath<Node>) {
-        if (nodePath.parentPath.isVariableDeclarator()
-          || nodePath.parentPath.isAssignmentExpression()
-          || (nodePath.parentPath.isCallExpression()) && ((nodePath.parentPath.get(
-            "arguments") as any) || [] as NodePath[]).indexOf(nodePath) > -1) {
+        if (
+          nodePath.parentPath.isVariableDeclarator() ||
+          nodePath.parentPath.isAssignmentExpression() ||
+          (nodePath.parentPath.isCallExpression() &&
+            (
+              (nodePath.parentPath.get("arguments") as any) ||
+              ([] as NodePath[])
+            ).indexOf(nodePath) > -1)
+        ) {
           if (!isPureAnnotated(nodePath.node.leadingComments)) {
-            const pureAnnotation = createComponentBlock(PURE_ANNOTATION)
+            const pureAnnotation = createComponentBlock(PURE_ANNOTATION);
 
             nodePath.replaceWith({
               ...nodePath.node,
               leadingComments: nodePath.node.leadingComments
                 ? nodePath.node.leadingComments.concat(pureAnnotation)
                 : [pureAnnotation],
-            })
+            });
           }
         }
       },
     },
   },
-})
+});
